2cb7d88b4026347ce5e8dc41e0c3b5dd0c010e8b
j.majurel <j.majurel@bpp-tech.com>
2017-07-28 18:56:09 +0100

    changes in the model, loading dat from input file

--- src/main/scala/App/Models/models.scala
+++ src/main/scala/App/Models/models.scala
@@ -1,14 +1,24 @@
 package com.montecarlo
 
 import org.apache.poi._
-import java.io.FileInputStream
+import java.io.File
+import java.io.FileNotFoundException
+import org.apache.poi.openxml4j.opc.OPCPackage
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
+import org.apache.poi.ss.usermodel.CellType
 
 trait Models extends GanttModel{ this: MVC =>
 
+  object InputFileKeyWord{
+    operation: String = "<op>"
+    preop: String = "<pre_op>"
+    startdate: String = "<start_date>"
+    bcdurationcustomer: String = "<day_c">
+  }
+
   class Model{
 
-    case class Operation(
+    /*case class Operation(
       name: String,
       predecessor: Operation,
       startdate: Date,
@@ -20,8 +30,9 @@ trait Models extends GanttModel{ this: MVC =>
       bcdayratecostbpp: Double,
       pdffunctioncost: String,
       pdffunctionduration: String
-    )
+    )*/
 
+    case class Operation(name: String)
 
     var gantmodel = new GanttModel()
 
@@ -30,28 +41,52 @@ trait Models extends GanttModel{ this: MVC =>
      */
     def load(){
 
-      val fis = new FileInputStream("Scenario_Operations_Overview_DEV.xlsx")
-      val wb = new XSSFWorkbook(fis)
-      val sheet = wb.getSheetAt(0)
-      val nbrow = sheet.getPhysicalNumberOfRows()
-      for (sheet.rowIterator){
-        println("value")
+      try{
+        val pkg = OPCPackage.open(new File("Scenario_Operations_Overview_DEV.xlsx"))
+        val wb = new XSSFWorkbook(pkg)
+        val sheet = wb.getSheetAt(0)
+        var row: Int = 0
 
+        //for(row <- 5 until sheet.getPhysicalNumberOfRows()){
+        for(row <- 5 to 6){
+
+          var currentrow = sheet.getRow(row)
+          var column: Int = 0
+
+          for(column <- 1 until currentrow.getLastCellNum()){
+
+            var currentcell = currentrow.getCell(column)
+            println("getCellTypeEnum():"+currentcell.getCellTypeEnum())
+
+            currentcell.getCellTypeEnum() match {
+              case CellType.STRING =>
+                println("New Operation object loaded:"+Operation(currentcell.getStringCellValue()))
+              case _ =>
+                println("data not a string")
+            }
+          }
+        }
+        pkg.close()
       }
-      println("nbrow in sheet:"+nbrow)
-      var row = sheet.getRow(5)
-      var cellop = row.getCell(13)
-      var cellpreop = row.getCell(14)
-      var cellduration = row.getCell(15)
-      var opname = cellop.getStringCellValue()
-      var oppreop = cellpreop.getStringCellValue()
-      // var opduration = cellduration.getStringCellValue()
-      //var op1 = Operation(id=1 , name:String, statedate:Date, duration:Int, cost:Double)
+      catch {
+        case ex: FileNotFoundException => {
+          println("Missing file exception")
+        }
+      }
+        /*println("nbrow in sheet:"+nbrow)
+        var row = sheet.getRow(5)
+        var cellop = row.getCell(13)
+        var cellpreop = row.getCell(14)
+        var cellduration = row.getCell(15)
+        var opname = cellop.getStringCellValue()
+        var oppreop = cellpreop.getStringCellValue()
+        // var opduration = cellduration.getStringCellValue()
+        //var op1 = Operation(id=1 , name:String, statedate:Date, duration:Int, cost:Double)
 
-      println("opname:"+opname)
-      println("oppreop:"+oppreop)
-      //println("opduration:"+opduration)
-      fis.close()
-    }
+        println("opname:"+opname)
+        println("oppreop:"+oppreop)
+        //println("opduration:"+opduration)
+        */
+        }
   }
 }
